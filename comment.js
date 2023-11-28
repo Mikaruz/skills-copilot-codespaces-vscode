ab896bf19a57dd15ccbe7f9f1860d4b5a3cd360b
Mikaruz <gersonsebas2019@gmail.com>
2023-11-28 17:35:28 -0500

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,57 @@
+// Import the 'http' module
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+// Create an HTTP server that responds with a "Hello, World!" message for all requests
+const server = http.createServer((req, res) => {
+    // Handling different routes
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentType = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    }[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            }
+            else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                res.end();
+            }
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+// Listen on port 3000, IP address defaults to 127.0.0.1
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://127.0.0.1:${port}/`);
+});
